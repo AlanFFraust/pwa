ab80e22e66323dd6f474bf9f21eb755fe1d4a791
Dola <AlanFFrausto@gmail.com>
2023-11-13 16:19:15 -0700

    modificado el sw

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
 const CACHE_NAME = 'conversor-currency-v1';
 const urlsToCache = [
     '/',
-    '/index.html',
-    '/manifest.json',
-    '/icon.png',
-    // Agrega aquí otros recursos que desees precachear
+    './index.html',
+    './manifest.json',
+    './icon.png',
+    './styles.css'
 ];
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache1 = await caches.open(CACHE_NAME);
+        cache1.addAll(urlsToCache);
+    })());
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
+self.addEventListener('fetch', event => {
+    event.respondWith((async () => {
+
         caches.match(event.request)
-            .then(function(response) {
+            .then(function (response) {
                 return response || fetch(event.request);
             })
-    );
+    })());
 });
+
