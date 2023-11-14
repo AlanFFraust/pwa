a9300e314dd19f996d86d9e0a3758ebe597f108a
Dola <AlanFFrausto@gmail.com>
2023-11-13 19:09:50 -0700

    .

--- sw.js
+++ sw.js
@@ -5,13 +5,47 @@ const urlsToCache = [
     './manifest.json',
     './icon.png',
     './styles.css',
-    './app.js',
-    './valores.js'
+    './app.js'
 ];
 const API_URL = 'https://v6.exchangerate-api.com/v6/47a2c19322df51ee86d4d0c4/latest/';
 const DYNAMIC_CACHE_NAME = 'memoria-cache-dinamica-v1';
-import valoresPredeterminados from './valores';
-
+const valoresPredeterminados = {
+    MXN: {
+        USD: 0.057,
+        MXN: 1,
+        EUR: 0.053,
+        JPY: 8.61,
+        GBP: 0.046
+    },
+    USD: {
+        MXN: 17.62,
+        USD: 1,
+        EUR: 0.93,
+        JPY: 151.75,
+        GBP: 0.81
+    },
+    EUR: {
+        MXN: 18.84,
+        USD: 1.07,
+        EUR: 1,
+        JPY: 128.79,
+        GBP: 0.87
+    },
+    JPY: {
+        MXN: 0.12,
+        USD: 0.007,
+        EUR: 0.008,
+        JPY: 1,
+        GBP: 0.005
+    },
+    GBP: {
+        MXN: 21.74,
+        USD: 1.23,
+        EUR: 1.15,
+        JPY: 186.15,
+        GBP: 1
+    }
+};
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
